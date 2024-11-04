2455b3e07137f8d9b4427f44f51e6cb2e2e626a1
renansilveiradesousamendonca <61990105+renansilveiradesousamendonca@users.noreply.github.com>
2024-11-04 13:15:44 -0300

    Add files via upload
    Signed-off-by: renansilveiradesousamendonca <61990105+renansilveiradesousamendonca@users.noreply.github.com>

---
+++ Saneamento_Basico_R/src/main/java/com/mycompany/saneamento_basico_r/Saneamento_Basico_R.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.saneamento_basico_r;
+
+/**
+ *
+ * @author renan
+ */
+public class Saneamento_Basico_R {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
